46e446009e55300bca4c68a260ba16aeba8ff1cf
Nabeel Mohammed <64881066+nabeelmohammed1@users.noreply.github.com>
2026-04-18 20:30:10 -0400

    cpp files that didnt push

---
+++ Source/AirShooter/FPSGamemode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FPSGamemode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AIRSHOOTER_API AFPSGamemode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
